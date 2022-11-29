7dbfa47940d2c07687c8f852625ce53ba09f9de0
devshehan <shehaan.avishka00@gmail.com>
2022-11-29 08:34:59 +0530

    database connect

--- traversyMVC/app/config/config.php
+++ traversyMVC/app/config/config.php
@@ -1,3 +1,11 @@
 <?php 
+    // DB params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'Car');
+
+
+
     define('APPROOT', dirname(dirname(dirname(__FILE__))));
 
